09edcc9dc57caf97d4567e8d9860aee2aaccf1ee
mohmadimran <pathanimrankhan308@gmail.com>
2026-02-20 14:39:24 +0530

    change service file

--- src/service.js
+++ src/service.js
@@ -18,22 +18,3 @@ const getActivityService = async ({ tenantId, cursor, limit }) => {
 };
 
 module.exports = { createActivityService, getActivityService };
-// const ActivityModel = require("./DbSchima")
-
-// const createActivityService = async(data)=>{
-//     return await ActivityModel.create(data)
-// }
-
-// const getActivityService = ({tenantId,cursor,limit})=>{
-
-// const query = tenantId;
-
-// if(cursor){
-//     query.createdAt = {$lt:new Date(cursor)}
-// }
-
-// return ActivityModel.find(query).sort({createdAt:-1}).select("tenantId actor activity createdAt").limit(Number(20))
-
-// }
-
-// module.exports = {createActivityService,getActivityService}
